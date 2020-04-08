cec155fcaca5b79430897c41de0d466c5737cc22
chilot <https://github.com/Raigyo>
2020-04-08 16:11:24 +0200

    Updates

---
+++ precache-manifest.2fbbc9645f740b40a811e0aecd57cf57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd5543443dcf0e327f03ab48be720a28",
+    "url": "/react-chat/index.html"
+  },
+  {
+    "revision": "4870aa133db419158592",
+    "url": "/react-chat/static/css/2.0161871a.chunk.css"
+  },
+  {
+    "revision": "949f9d8aca8438c55d06",
+    "url": "/react-chat/static/css/main.c34a4f03.chunk.css"
+  },
+  {
+    "revision": "4870aa133db419158592",
+    "url": "/react-chat/static/js/2.6b2ff328.chunk.js"
+  },
+  {
+    "revision": "949f9d8aca8438c55d06",
+    "url": "/react-chat/static/js/main.2ed21a2e.chunk.js"
+  },
+  {
+    "revision": "49fde9a2889aba467256",
+    "url": "/react-chat/static/js/runtime~main.14e77f4a.js"
+  }
+]);
